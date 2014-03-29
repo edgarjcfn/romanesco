db751aa77519259549cf7f984939a0683b6898c7
edgarjcfn <edgar.neto@wooga.com>
2014-03-29 21:59:39 +0100

    Added todo view

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,18 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Expressa',
+    todos: [ 
+      { description: "Buy eggs",
+        done: false
+      },
+      { description: "Write next blog post",
+        done: false
+      }
+      { description: "Write that todo app",
+        done: true 
+      }
+    ]
+  });
 };
